6ec94c884dae96ee08a032d685de4cb45294adf6
David Konsumer <konsumer@jetboystudio.com>
2019-04-18 16:46:49 -0700

    initial commit

---
+++ styleguide.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  sections: [
+    {
+      name: 'Introduction',
+      content: 'docs/intro.md'
+    },
+    {
+      name: 'Components',
+      sections: [
+        {
+          name: 'Button',
+          content: 'docs/buttons.md'
+        }
+      ]
+    },
+    {
+      name: 'Concepts',
+      content: 'docs/concepts.md',
+      sections: [
+        {
+          name: 'Colors',
+          content: 'docs/colors.md'
+        }
+      ]
+    }
+  ]
+}
